2527f34963b02c181b21616857e03034aafce211
void_lhf <void_lhf@qq.com>
2024-11-19 02:18:19 +0800

    2024年 11月 19日 星期二 02:18:19 CST

--- clash2sfa/main.go
+++ clash2sfa/main.go
@@ -14,27 +14,23 @@ import (
 )
 
 func main() {
-	// 设置端口，默认为 8090
 	port := ":8090"
 	if p := os.Getenv("port"); p != "" {
 		port = p
 	}
 
-	// 设置日志级别
 	levels := os.Getenv("level")
 	leveln, err := strconv.Atoi(levels)
 	if err != nil {
 		leveln = -4
 	}
 
-	// 配置日志级别
 	level := &slog.LevelVar{}
 	level.Set(slog.Level(leveln))
 	h := slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
 		Level: level,
 	})
 
-	// 创建 HTTP 服务器
 	s := http.Server{
 		ReadTimeout:       30 * time.Second,
 		WriteTimeout:      30 * time.Second,
@@ -42,14 +38,5 @@ func main() {
 		Addr:              port,
 		Handler:           handler.SetMux(h),
 	}
-
-	// 启动 Web 服务并将其放到后台
-	go func() {
-		if err := s.ListenAndServe(); err != nil {
-			fmt.Println("Server failed:", err)
-		}
-	}()
-
-	// 主程序保持运行，不会阻塞
-	select {}  // 阻塞主程序直到程序结束
+	fmt.Println(s.ListenAndServe())
 }
