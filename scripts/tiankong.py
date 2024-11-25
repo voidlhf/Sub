f4edb7e894c92455ebbe9be73cd7c6736a885da2
voidlhf <void_lhf@qq.com>
2024-11-26 00:24:00 +0800

    2024年 11月 26日 星期二 00:24:00 CST

---
+++ scripts/tiankong.py
@@ -0,0 +1,31 @@
+import os
+import requests
+import sys
+
+# 将工作目录切换到脚本所在的目录
+os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
+
+# 定义基础 URL 和拼接参数
+base_url = "https://clash2sfa.xmdhs.com/sub"
+config_url = "https://raw.githubusercontent.com/voidlhf/Sub/refs/heads/branch1/config.json.template"
+sub_url = "https://aini.200566.xyz/tzjz55/api/file/class"
+
+# 拼接完整的 URL
+url = f"{base_url}?configurl={config_url}&outFields=0&sub={sub_url}"
+
+save_path = "../singbox/tiankong.json"
+
+# 创建保存目录（如果目录不存在）
+os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+try:
+    # 下载 JSON 文件
+    response = requests.get(url)
+    response.raise_for_status()  # 检查请求状态
+    with open(save_path, "w", encoding="utf-8") as file:
+        file.write(response.text)
+    print(f"下载完成，文件已保存至 {save_path}")
+
+
+except requests.exceptions.RequestException as e:
+    print(f"下载失败：{e}")
