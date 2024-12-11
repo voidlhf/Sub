d2058dadec894e1cbb2dc454a0d8268f1eedd3dd
voidlhf <void_lhf@qq.com>
2024-12-11 10:28:28 +0800

    2024年 12月 11日 星期三 10:28:28 CST

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 python ./scripts/dcj.py
-#python ./scripts/bzy.py
 python ./scripts/air.py
 python ./scripts/guanxi.py
-#python ./scripts/gbtg.py
 python ./scripts/hlsg.py
 python ./scripts/tiankong.py
+python ./scripts/skr.py
