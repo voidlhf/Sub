1a6a305609b716fa9929addf9fa3b7b201680463
voidlhf <void_lhf@qq.com>
2025-01-02 20:21:07 +0800

    2025年 01月 02日 星期四 20:21:07 CST

--- run_scripts.sh
+++ run_scripts.sh
@@ -3,7 +3,7 @@
 # mihomo
 python scripts/mihomo-remote-generate.py "https://suo.st/d2Y76gj" ../mihomo/air.yaml
 
-# python scripts/mihomo-remote-generate.py "http://api.scyu.xyz/sub?token=WxMk2VVGqNHuforQPneQ7g&tag=clash" ../mihomo/dx.yaml
+#python scripts/mihomo-remote-generate.py "http://api.scyu.xyz/sub?token=WxMk2VVGqNHuforQPneQ7g&tag=clash" ../mihomo/dx.yaml
 
 python scripts/mihomo-remote-generate.py "https://sublinks.52cloud.eu.org/api/v1/client/subscribe?token=b26fe554f300d7c9573d858c11b50a0b" ../mihomo/bzy.yaml
 
@@ -11,7 +11,7 @@ python scripts/mihomo-remote-generate.py "https://trojans.trevely.us.kg/Trevely"
 
 python scripts/mihomo-remote-generate.py "https://ly.ccwink.cc/api/v1/client/subscribe?token=c2bc3bc950ec1e5d22bc9f45716a28a2" ../mihomo/ly.yaml
 
-python scripts/mihomo-remote-generate.py "https://suo.st/E2VJf4t" ../mihomo/skr.yaml
+#python scripts/mihomo-remote-generate.py "https://suo.st/E2VJf4t" ../mihomo/skr.yaml
 
 python scripts/mihomo-remote-generate.py "https://aini.200566.xyz/tzjz55/download/collection/love?target=ClashMeta" ../mihomo/tk1.yaml
 
@@ -24,13 +24,13 @@ python scripts/mihomo-remote-generate.py "https://aini.200566.xyz/tzjz55/downloa
 # singbox
 python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://suo.st/d2Y76gj" ../singbox/air.json
 
-python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://suo.st/E2VJf4t" ../singbox/skr.json
+#python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://suo.st/E2VJf4t" ../singbox/skr.json
 
 python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://ly.ccwink.cc/api/v1/client/subscribe?token=c2bc3bc950ec1e5d22bc9f45716a28a2" ../singbox/ly.json
 
-python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://raw.githubusercontent.com/voidlhf/Sub/refs/heads/branch1/mihomo/matefox.yaml" ../singbox/matefox.json
+#python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://raw.githubusercontent.com/voidlhf/Sub/refs/heads/branch1/mihomo/matefox.yaml" ../singbox/matefox.json
 
-python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://raw.githubusercontent.com/voidlhf/Sub/refs/heads/branch1/mihomo/sl.yaml" ../singbox/sl.json
+#python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://raw.githubusercontent.com/voidlhf/Sub/refs/heads/branch1/mihomo/sl.yaml" ../singbox/sl.json
 
 python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://raw.githubusercontent.com/ripaojiedian/freenode/main/clash" ../singbox/ripao.json
 
