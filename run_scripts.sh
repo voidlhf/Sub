56f6212a9fa2e40d03150944e27036e40511f329
voidlhf <void_lhf@qq.com>
2025-01-04 01:02:54 +0800

    2025年 01月 04日 星期六 01:02:54 CST

--- run_scripts.sh
+++ run_scripts.sh
@@ -2,44 +2,27 @@
 
 # mihomo
 python scripts/mihomo-remote-generate.py "https://suo.st/d2Y76gj" ../mihomo/air.yaml
-
-#python scripts/mihomo-remote-generate.py "http://api.scyu.xyz/sub?token=WxMk2VVGqNHuforQPneQ7g&tag=clash" ../mihomo/dx.yaml
-
 python scripts/mihomo-remote-generate.py "https://sublinks.52cloud.eu.org/api/v1/client/subscribe?token=b26fe554f300d7c9573d858c11b50a0b" ../mihomo/bzy.yaml
-
 python scripts/mihomo-remote-generate.py "https://trojans.trevely.us.kg/Trevely" ../mihomo/trevely.yaml
-
 python scripts/mihomo-remote-generate.py "https://ly.ccwink.cc/api/v1/client/subscribe?token=c2bc3bc950ec1e5d22bc9f45716a28a2" ../mihomo/ly.yaml
-
-#python scripts/mihomo-remote-generate.py "https://suo.st/E2VJf4t" ../mihomo/skr.yaml
-
 python scripts/mihomo-remote-generate.py "https://aini.200566.xyz/tzjz55/download/collection/love?target=ClashMeta" ../mihomo/tk1.yaml
-
 python scripts/mihomo-remote-generate.py "https://aini.200566.xyz/tzjz55/download/collection/tzjz66?target=ClashMeta" ../mihomo/tk2.yaml
-
 python scripts/mihomo-remote-generate.py "http://tg.hwzy0.ggff.net/sub?token=oLKUUBC7cJPmouRzlnw2bg&tag=clash" ../mihomo/hwzy0.yaml
-
 python scripts/mihomo-remote-generate.py "https://aini.200566.xyz/tzjz55/download/xinniankuaile?target=ClashMeta" ../mihomo/xnkl.yaml
-
 python scripts/mihomo-remote-generate.py "https://aini.200566.xyz/tzjz55/download/beiyongjiedian" ../mihomo/byjd.yaml
+python scripts/mihomo-remote-generate.py "http://38.54.50.246:10002/" ../mihomo/rzy.yaml
+python scripts/mihomo-remote-generate.py "https://raw.githubusercontent.com/ripaojiedian/freenode/main/clash" ../mihomo/ripao.yaml
+python scripts/mihomo-remote-generate.py "https://raw.githubusercontent.com/go4sharing/sub/main/sub.yaml" ../mihomo/go4sharing.yaml
+python scripts/mihomo-remote-generate.py "https://raw.githubusercontent.com/zhangkaiitugithub/passcro/main/speednodes.yaml" ../mihomo/speednodes.yaml
 
 # singbox
 python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://suo.st/d2Y76gj" ../singbox/air.json
-
-#python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://suo.st/E2VJf4t" ../singbox/skr.json
-
 python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://ly.ccwink.cc/api/v1/client/subscribe?token=c2bc3bc950ec1e5d22bc9f45716a28a2" ../singbox/ly.json
-
-#python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://raw.githubusercontent.com/voidlhf/Sub/refs/heads/branch1/mihomo/matefox.yaml" ../singbox/matefox.json
-
-#python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://raw.githubusercontent.com/voidlhf/Sub/refs/heads/branch1/mihomo/sl.yaml" ../singbox/sl.json
-
 python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://raw.githubusercontent.com/ripaojiedian/freenode/main/clash" ../singbox/ripao.json
-
 python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://raw.githubusercontent.com/go4sharing/sub/main/sub.yaml" ../singbox/go4sharing.json
-
 python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://raw.githubusercontent.com/zhangkaiitugithub/passcro/main/speednodes.yaml" ../singbox/speednodes.json
-
 python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://aini.200566.xyz/tzjz55/download/collection/love?target=ClashMeta" ../singbox/tk1.json
-
 python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://aini.200566.xyz/tzjz55/download/collection/tzjz66?target=ClashMeta" ../singbox/tk2.json
+python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://aini.200566.xyz/tzjz55/download/xinniankuaile?target=ClashMeta" ../singbox/xnkl.json
+python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=https://aini.200566.xyz/tzjz55/download/beiyongjiedian" ../singbox/byjd.json
+python scripts/singbox-remote-generate.py "https://clash2sfa.xmdhs.com/sub?sub=http://38.54.50.246:10002/" ../singbox/rzy.json
