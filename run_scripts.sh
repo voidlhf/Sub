05dd1f6149feecb9ca3a191dd722b53253df4dcb
voidlhf <void_lhf@qq.com>
2025-05-10 20:15:22 +0800

    2025年 05月 10日 星期六 20:15:22 CST

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 python scripts/xf.py
-python -u gen_configs.py --json "sub-zs.json"
-python -u gen_configs.py --json "sub.json"
+#python -u gen_configs.py --json "sub-zs.json"
+#python -u gen_configs.py --json "sub.json"
+python -u gen_configs_2.py --json "sub.json"
